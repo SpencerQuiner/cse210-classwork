cfb26b2dfcb9d965d03ef5a6406565e0a10aed50
SpencerQuiner <spencerquiner@gmail.com>
2023-09-13 13:53:26 +0100

    Prep 2 complete with stretch goals

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,67 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Please enter your grade percentage? ");
+        string response = Console.ReadLine();
+        int grade = int.Parse(response);
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80 && grade < 90)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        string sign = "";
+
+        if (grade % 10 >= 7)
+        {
+            sign = "+";
+        }
+        else if (grade % 10 <= 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        if (letter == "A" && sign == "+")
+        {
+            sign = "";
+        }
+        if (letter == "F")
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}{sign}.");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't Pass.");
+        }
+
+
     }
 }
